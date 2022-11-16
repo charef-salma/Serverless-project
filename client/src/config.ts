e3a20df3c1e7be71a632feed9dd3678907cd7dbf
salma-charef <charefsalma1998@gmail.com>
2022-11-16 22:43:48 +0100

    ff

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '16drc4yfxj'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-pmhej6dx.us.auth0.com',
+  clientId: 'WF7ZCBXrsnJfwe4hy2ANQzLR2b110BFe',
+  callbackUrl: 'http://localhost:3000/callback'
+};
